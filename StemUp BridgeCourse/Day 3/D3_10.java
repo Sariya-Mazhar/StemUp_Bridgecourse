944ebcd4a06e5f58cedd4f13d838c40b791c4614
Sariya-Mazhar <sariyamazhar27@gmail.com>
2025-06-28 13:52:31 +0530

    commit

--- StemUp BridgeCourse/Day 3/D3_10.java
+++
@@ -1,8 +0,0 @@
-public class D3_10 {
-        public static void main(String[] args){
-         for (int i=1; i <=5; i++){
-              System.out.print("*");
-     
-         }
-        }
-     }
